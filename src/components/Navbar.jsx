b7325588081e8c47e2c26e9b73771d01f8fc5b4c
Marianne <marianne.garridom@gmail.com>
2023-11-15 14:23:38 -0600

    estilos nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import "./style.css";
 import { useNavigate } from "react-router-dom";
 import { handleDirection } from "utils/navigationUtils";
+import logo from "img/udgvirtual_logo.png";
 
 export const Navbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleDownload = () => {
     try {
@@ -23,11 +24,20 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar">
+      <img src={logo} alt="logo" />
       <button onClick={() => handleDirection("home", navigate)}>Inicio</button>
-      <button onClick={() => handleDirection("doc", navigate)}>Documentos</button>
-      <button onClick={handleDownload}>Descargar Zip</button>
-      <button onClick={() => handleDirection("modifications", navigate)}>Modificaciones</button>
-      <button onClick={() => handleDirection("system", navigate)}>Sistema</button>
+      <button onClick={() => handleDirection("doc", navigate)}>
+        Documentos
+      </button>
+      <button onClick={handleDownload} id="download">
+        Descargar Zip
+      </button>
+      <button onClick={() => handleDirection("modifications", navigate)}>
+        Modificaciones
+      </button>
+      <button onClick={() => handleDirection("system", navigate)}>
+        Sistema
+      </button>
     </nav>
   );
 };
