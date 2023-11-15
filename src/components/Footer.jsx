51431e703ce005ff0533901180995430434c38fa
Marianne <marianne.garridom@gmail.com>
2023-11-15 14:34:01 -0600

    Estilos Nav y Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,16 @@
 export const Footer = () => {
-    return <h2>Footer</h2>;
+  return (
+    <footer>
+      <div>
+        <p>Marianne Garrido Minutti</p>
+        <a href="mailto:marianne.garridom@gmail.com">
+          marianne.garridom@gmail.com
+        </a>
+      </div>
+      <div>
+        <p>Proyecto II: Producto integrador. El final del proceso</p>
+        <p>2023</p>
+      </div>
+    </footer>
+  );
 };
