129aa096db607b30d84a53a3c4c7ffc99f039844
Marianne <marianne.garridom@gmail.com>
2023-11-15 19:15:48 -0600

    table component

--- src/pages/documents/ProyectDocuments.jsx
+++ src/pages/documents/ProyectDocuments.jsx
@@ -1,9 +1,8 @@
 import "./style.css";
-import { ROLES } from "data/doc"
-
+import { ROLES, SPRINTS_CALENDAR, USER_HISTORY } from "data/doc";
+import { Table } from "./Table";
 
 export const ProyectDocuments = () => {
-console.log(ROLES);
 
   return (
     <div className="doc">
@@ -15,7 +14,8 @@ console.log(ROLES);
         proyecto, por lo que los roles asignados están basados en personajes
         ficticios
       </span>
-
+      <br />
+      <Table table={ROLES} />
       <h2>CALENDARIZACIÓN DE LOS SPRINT</h2>
       <p>
         <i>
@@ -23,63 +23,7 @@ console.log(ROLES);
           Backlog)
         </i>
       </p>
-      <div className="table">
-        <tr>
-          <td>Sprint</td>
-          <td>fecha inicio </td>
-          <td>fecha fin</td>
-          <td>fecha revisión</td>
-          <td>estado</td>
-        </tr>
-
-        <tr>
-          <td>S001</td>
-          <td>28/08/2023 </td>
-          <td>5/09/2023</td>
-          <td>07/09/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-
-        <tr>
-          <td>S002</td>
-          <td>08/09/2023</td>
-          <td>15/09/2023</td>
-          <td>17/09/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-
-        <tr>
-          <td>S003</td>
-          <td>25/09/2023</td>
-          <td>29/09/2023</td>
-          <td>04/10/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-
-        <tr>
-          <td>S004</td>
-          <td>2/10/2023</td>
-          <td>6/10/2023</td>
-          <td>11/10/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-
-        <tr>
-          <td>S005</td>
-          <td>9/10/2023</td>
-          <td>13/10/2023</td>
-          <td>18/10/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-
-        <tr>
-          <td>S006</td>
-          <td>16/10/2023</td>
-          <td>20/10/2023</td>
-          <td>29/10/2023</td>
-          <td className="complete">Completado</td>
-        </tr>
-      </div>
+      <Table table={SPRINTS_CALENDAR} />
     </div>
   );
 };
---
+++ src/pages/documents/Table.jsx
@@ -0,0 +1,20 @@
+export const Table = ({ table }) => {
+  return (
+    <div className="table">
+      <tr>
+        {table.headers.map((element, index) => {
+          return <td key={index}>element</td>;
+        })}
+      </tr>
+      {table.columns.map((column, index) => {
+        return (
+          <tr key={index}>
+            {column.map((row, subIndex) => {
+              return <td key={subIndex}>{row}</td>;
+            })}
+          </tr>
+        );
+      })}
+    </div>
+  );
+};
