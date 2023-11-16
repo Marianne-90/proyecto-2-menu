6c1e1a85944b32fd8affcaa9dfecb68958718b2b
Marianne <marianne.garridom@gmail.com>
2023-11-15 19:49:16 -0600

    referencias

--- src/pages/documents/ProyectDocuments.jsx
+++ src/pages/documents/ProyectDocuments.jsx
@@ -26,6 +26,25 @@ export const ProyectDocuments = () => {
       <Table table={SPRINTS_CALENDAR} />
       <h2>HISTORIAS DE USUARIO</h2>
       <UserHistory table={USER_HISTORY} />
+      <h2>PRODUCT BACKLOG</h2>
+      {/* <Table table={ROLES} /> */}
+      <h2>FORMATO SPRINT:</h2>
+      {/* <Table table={ROLES} /> */}
+      <h2>REFERENCIAS</h2>
+      <ul className="referencias">
+        <li>Menzinsky A. (2018) “Historias de Usuario”. Scrum Manager</li>
+        <li>Schwaber K. (2013) “La Guía de Scrum”</li>
+        <li>
+          CECyT Digital Harbor. (6 abr. 2018). Ejemplo de Product Backlog
+          (Taller Mecanico). [Archivo de video].
+          https://www.youtube.com/watch?time_continue=5&v=hIPHROMebRs&embeds_referring_euri=https%3A%2F%2Fdesarrolloweb.udgvirtual.udg.mx%2F&embeds_referring_origin=https%3A%2F%2Fdesarrolloweb.udgvirtual.udg.mx&source_ve_path=NzY3NTg&feature=emb_yt_watermark
+        </li>
+        <li>
+          JorgeRuizAgile (11 ago. 2018). El Product Backlog de forma sencilla |
+          Scrum. [Archivo de video]. Recuperado de
+          https://www.youtube.com/watch?v=JuIVHk1wvcw&t=2s&ab_channel=JorgeRuizAgile
+        </li>
+      </ul>
     </div>
   );
 };
